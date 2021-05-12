cd32ceab7c8829d513e2689738c69aa897f6e808
Ingebrigt <ing.hovind@hotmail.com>
2021-05-12 13:44:19 +0200

    initial commit

---
+++ backend/reserved/src/main/java/idatt2105/erlinssl/ikhovind/reserved/ReservedApplication.java
@@ -0,0 +1,13 @@
+package idatt2105.erlinssl.ikhovind.reserved;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservedApplication.class, args);
+	}
+
+}
