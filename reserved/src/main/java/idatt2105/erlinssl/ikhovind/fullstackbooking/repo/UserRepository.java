e665d623cfc89eacd17d70f2ddd26e63439ffda7
ikhovind <ing.hovind@hotmail.com>
2021-05-13 11:36:54 +0200

    https and https redirect

--- reserved/src/main/java/idatt2105/erlinssl/ikhovind/fullstackbooking/repo/UserRepository.java
+++ reserved/src/main/java/idatt2105/erlinssl/ikhovind/fullstackbooking/repo/UserRepository.java
@@ -4,6 +4,7 @@ import idatt2105.erlinssl.ikhovind.fullstackbooking.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
