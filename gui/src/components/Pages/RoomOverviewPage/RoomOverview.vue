14a265ff2c262fdb4a5ffab96c2060b42b1c26d3
ikhovind <ing.hovind@hotmail.com>
2021-05-19 23:31:17 +0200

    admin can now probably edit timestamps for users

--- gui/src/components/Pages/RoomOverviewPage/RoomOverview.vue
+++ gui/src/components/Pages/RoomOverviewPage/RoomOverview.vue
@@ -37,7 +37,19 @@
           <th>Til</th>
         </tr>
       </table>
-      <button :disabled="selectedIndex === -1">Rediger reservasjon</button>
+      <button :disabled="selectedIndex === -1" @click="showEditModal = true">Rediger reservasjon</button>
+      <div v-if="showEditModal" id="editReservationModal">
+        <div id="editSection">
+          <div class="modal-mask">
+            <div class="modal-wrapper">
+              <div class="modal-container">
+              <EditReservation :reservation="reservations[this.selectedIndex]" v-on:createdReservation="closeModal()"></EditReservation>
+                <button @click="closeModal()">Avbryt</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <label for="userInfo">Brukerinfo</label>
       <table id="userInfo">
         <tr>
@@ -53,14 +65,16 @@
 <script>
 import Header from "@/components/Pages/Common/Header";
 import EditRoomModal from "@/components/Pages/Common/EditRoomModal";
+import EditReservation from "@/components/Pages/Common/EditReservation";
 export default {
   name: "RoomOverview",
-  components: {EditRoomModal, Header},
+  components: {EditReservation, EditRoomModal, Header},
   created() {
     this.fetchReservations();
   },
   data () {
     return {
+      showEditModal: false,
       reservations: [],
       selectedIndex: -1
     }
@@ -68,6 +82,9 @@ export default {
   methods: {
     test() {
       console.log("woho");
+    },
+    closeModal() {
+       this.showEditModal = false;
     },
     isAdmin() {
       return localStorage.getItem("userType") !== "0";
@@ -178,5 +195,48 @@ export default {
   text-decoration: none;
   font-size: 16px;
 }
+
+.modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: table;
+  transition: opacity 0.3s ease;
+}
+
+.modal-wrapper {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.modal-container {
+  width: 40%;
+  height: 379px;
+  margin: 0px auto;
+  padding: 20px 30px;
+  background-color: #fff;
+  border-radius: 2px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  transition: all 0.3s ease;
+  font-family: Helvetica, Arial, sans-serif;
+}
+
+.modal-header h3 {
+  margin-top: 0;
+  color: #42b983;
+}
+
+.modal-body {
+  margin: 20px 0;
+}
+
+.modal-default-button {
+  float: right;
+}
+
 
 </style>
